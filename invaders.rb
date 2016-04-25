eac054e403048bdef65bb66a2259f66c2113aac7
Chad Jemmett <chad.jemmett@gmail.com>
2016-04-24 18:47:19 -0600

    have the window working

--- invaders.rb
+++ invaders.rb
@@ -1,1 +1,26 @@
 require 'gosu'
+
+
+FONT_COLOR = 0xff_ffff00
+
+class SpaceInvader < Gosu::Window
+  def initialize
+    super 640, 480
+    self.caption = "Space Invaders"
+  end
+
+
+  def update
+  end
+
+  def draw
+  end
+
+  def button_down(id)
+  end
+
+end
+
+
+window = SpaceInvader.new
+window.show
