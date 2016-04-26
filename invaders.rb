054270018cbc304fc3a007cad445fc820c9276ba
Chad Jemmett <chad.jemmett@gmail.com>
2016-04-26 14:11:40 -0600

    firing bullets now works! woo

--- invaders.rb
+++ invaders.rb
@@ -1,6 +1,4 @@
 require 'gosu'
-
-
 FONT_COLOR = 0xff_ffff00
 
 class SpaceInvader < Gosu::Window
@@ -9,10 +7,14 @@ class SpaceInvader < Gosu::Window
     self.caption = "Space Invaders"
     @message = Gosu::Font.new(20)
     @player = Player.new
+    @bullets = Array.new
   end
 
-
   def update
+    @bullets.each do |item|
+      item.y -= 3
+    end
+    @bullets.reject! {|bullet| bullet.y == 0}
     @player.right if Gosu::button_down?(Gosu::KbRight) unless @player.collision?(640, 450)
     @player.left if Gosu::button_down?(Gosu::KbLeft) unless @player.collision?(0, 450)
     close if Gosu::button_down?(Gosu::KbEscape)
@@ -20,8 +22,8 @@ class SpaceInvader < Gosu::Window
 
   def button_down(id)
     if id == Gosu::KbSpace
-      @player.ammo[0].update(@player.x, @player.y)
-      #bullet firing code goes here.
+      @bullets.push(Bullet.new(@player.x, @player.y))
+      puts @bullets.length
     end
   end
 
@@ -29,11 +31,8 @@ class SpaceInvader < Gosu::Window
     @message.draw("Player X => #{@player.x} - Player Y => #{@player.y}", 10, 30, FONT_COLOR)
     @message.draw("Distance from 0 => #{Gosu::distance(@player.x, @player.y, 320, 0)}", 10, 60, FONT_COLOR)
     @player.draw
-    @player.ammo[0].draw
-    #@player.ammo.each {|item| draw.item}
+    @bullets.each {|item| item.draw}
   end
-
-
 end
 
 class Player
@@ -41,10 +40,8 @@ class Player
   def initialize
     @x = 320
     @y = 450
-    @ammo = Array.new(5) {Bullet.new}
     @sprite = Gosu::Image.new("media/icon.png")
   end
-
 
   def draw
     @sprite.draw_rot(@x, @y, 1, 0)
@@ -58,9 +55,6 @@ class Player
     @x += 7
   end
 
-  def fire_bullet
-  end
-
   def collision?(barrier_x, barrier_y)
     #calculate distanct between cordinates and barrier coordinates.
     (@x.between?(barrier_x, barrier_x + 30) and @y.between?(barrier_y, barrier_y + 30)) || ((@x + 30).between?(barrier_x, barrier_x +30) and (@y + 30).between?(barrier_y, barrier_y + 30))
@@ -68,26 +62,18 @@ class Player
 end
 
 class Bullet
-  attr_accessor :x, :y, :firing
-  def initialize
-    @x = 0
-    @y = 0
+  attr_accessor :x, :y
+  def initialize(x, y)
+    @x = x
+    @y = y
     @bullet = Gosu::Font.new(20)
   end
-
 
   def draw
     @bullet.draw("i", @x, @y, FONT_COLOR)
   end
 
-  def update(player_x, player_y)
-    @x = player_x
-    @y = player_y
-  end
-
-
   def collision?(barrier_x, barrier_y)
-    #calculate distanct between cordinates and barrier coordinates.
     (@x.between?(barrier_x, barrier_x + 30) and @y.between?(barrier_y, barrier_y + 30)) || ((@x + 30).between?(barrier_x, barrier_x +30) and (@y + 30).between?(barrier_y, barrier_y + 30))
   end
 end
